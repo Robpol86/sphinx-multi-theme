3cbd6a93a91b325a72329436509dbf1dd02c66db
Robpol86 <robpol86@gmail.com>
2022-04-05 17:31:48 -0500

    relpaths

--- sphinx_multi_theme/utils.py
+++ sphinx_multi_theme/utils.py
@@ -40,14 +40,20 @@ def modify_forked_sphinx_app(app: Sphinx, subdir: str):
     # Set the output directory.
     new_outdir = os.path.join(old_outdir, subdir)
     ensuredir(new_outdir)
-    log.info("%sChanging outdir from %s to %s", LOGGING_PREFIX, old_outdir, new_outdir)
+    common_prefix = os.path.dirname(os.path.commonprefix([old_outdir, new_outdir]))
+    rel_old = os.path.relpath(old_outdir, common_prefix)
+    rel_new = os.path.relpath(new_outdir, common_prefix)
+    log.info("%sChanging outdir from %r to %r", LOGGING_PREFIX, rel_old, rel_new)
     app.outdir = new_outdir
 
     # Set the doctree directory.
     new_doctreedir = old_doctreedir.replace(old_outdir, new_outdir)
     if new_doctreedir == old_doctreedir:
         new_doctreedir = os.path.join(old_doctreedir, subdir)
-    log.info("%sChanging doctreedir from %s to %s", LOGGING_PREFIX, old_doctreedir, new_doctreedir)
+    common_prefix = os.path.dirname(os.path.commonprefix([old_doctreedir, new_doctreedir]))
+    rel_old = os.path.relpath(old_doctreedir, common_prefix)
+    rel_new = os.path.relpath(new_doctreedir, common_prefix)
+    log.info("%sChanging doctreedir from %r to %r", LOGGING_PREFIX, rel_old, rel_new)
     app.doctreedir = new_doctreedir
 
     # Exit after Sphinx finishes building before it sends Python up the call stack (e.g. during sphinx.testing).
