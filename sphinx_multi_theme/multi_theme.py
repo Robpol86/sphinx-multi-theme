9e25320dd4c1fb37db6c1837cc7098261339e614
Robpol86 <robpol86@gmail.com>
2022-04-07 19:18:18 -0500

    Update deps. (#10)
    Remove old deprecation warning silencer, no longer impacts pytest.
    
    Moved module variables into utils.py for future use to avoid circular
    imports.
    
    Remove pragma no cover comment, no cheating, what was I thinking lol.

--- sphinx_multi_theme/multi_theme.py
+++ sphinx_multi_theme/multi_theme.py
@@ -20,15 +20,8 @@ from sphinx.application import Sphinx
 from sphinx.config import Config
 from sphinx.util import logging
 
-from sphinx_multi_theme import __version__
+from sphinx_multi_theme import __version__, utils
 from sphinx_multi_theme.theme import MultiTheme
-
-CONFIG_NAME_INTERNAL_THEMES = "multi_theme__INTERNAL__MultiTheme"
-CONFIG_NAME_PRINT_FILES = "multi_theme_print_files"
-CONFIG_NAME_PRINT_FILES_STYLE = "multi_theme_print_files_style"
-LOGGING_PREFIX = "🍴 "
-SPHINX_CONNECT_PRIORITY_FLATTEN_HTML_THEME = 1
-SPHINX_CONNECT_PRIORITY_PRINT_FILES = 999
 
 
 def flatten_html_theme(_: Sphinx, config: Config):
@@ -44,12 +37,12 @@ def flatten_html_theme(_: Sphinx, config: Config):
         active_theme_name = multi_theme_instance.active.name
     except AttributeError:
         log = logging.getLogger(__name__)
-        log.warning("%sSphinx config value for `html_theme` not a %s instance", LOGGING_PREFIX, MultiTheme.__name__)
+        log.warning("%sSphinx config value for `html_theme` not a %s instance", utils.LOGGING_PREFIX, MultiTheme.__name__)
         return
 
     # Update config.
     config["html_theme"] = active_theme_name
-    config[CONFIG_NAME_INTERNAL_THEMES] = multi_theme_instance
+    config[utils.CONFIG_NAME_INTERNAL_THEMES] = multi_theme_instance
 
     # Support ReadTheDocs hosted docs.
     html_context_keys: List[Tuple[str, str]] = []
@@ -68,14 +61,14 @@ def print_files(app: Sphinx, exc: Exception):
     :param exc: Exception raised during Sphinx build process, may be unrelated to this library.
     """
     if exc:
-        return  # pragma: no cover
-    if not app.config[CONFIG_NAME_PRINT_FILES]:
+        return
+    if not app.config[utils.CONFIG_NAME_PRINT_FILES]:
         return
     log = logging.getLogger(__name__)
     print(flush=True)  # https://github.com/readthedocs/readthedocs-sphinx-ext/blob/2.1.5/readthedocs_ext/readthedocs.py#L270
     output = seedir(
         app.outdir,
-        style=app.config[CONFIG_NAME_PRINT_FILES_STYLE],
+        style=app.config[utils.CONFIG_NAME_PRINT_FILES_STYLE],
         printout=False,
         first="folders",
         sort=True,
@@ -92,9 +85,9 @@ def setup(app: Sphinx) -> Dict[str, str]:
 
     :returns: Extension version.
     """
-    app.add_config_value(CONFIG_NAME_INTERNAL_THEMES, None, "html")
-    app.add_config_value(CONFIG_NAME_PRINT_FILES, False, "")
-    app.add_config_value(CONFIG_NAME_PRINT_FILES_STYLE, "emoji" if os.sep == "/" else "dash", "")
-    app.connect("build-finished", print_files, priority=SPHINX_CONNECT_PRIORITY_PRINT_FILES)
-    app.connect("config-inited", flatten_html_theme, priority=SPHINX_CONNECT_PRIORITY_FLATTEN_HTML_THEME)
+    app.add_config_value(utils.CONFIG_NAME_INTERNAL_THEMES, None, "html")
+    app.add_config_value(utils.CONFIG_NAME_PRINT_FILES, False, "")
+    app.add_config_value(utils.CONFIG_NAME_PRINT_FILES_STYLE, "emoji" if os.sep == "/" else "dash", "")
+    app.connect("build-finished", print_files, priority=utils.SPHINX_CONNECT_PRIORITY_PRINT_FILES)
+    app.connect("config-inited", flatten_html_theme, priority=utils.SPHINX_CONNECT_PRIORITY_FLATTEN_HTML_THEME)
     return dict(version=__version__)
