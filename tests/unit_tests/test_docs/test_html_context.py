8d86c8cb7685953423d00e17b1249c48e963c0ec
Robpol86 <robpol86@gmail.com>
2022-04-11 15:54:29 -0500

    No fail_under, freshenv, subprocess. (#12)
    Removing fail_under and Coverage plugin, broke pytest-cov subprocess.
    
    Enabling freshenv in Sphinx tests.
    
    Using subproces to test exceptions. Using sphinx.testing will fail when
    forking due to config-inited causing forks when app is created and
    before app.build() is called, which is responsible for calling
    os._exit().

--- tests/unit_tests/test_docs/test_html_context.py
+++ tests/unit_tests/test_docs/test_html_context.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import pytest
 
 
-@pytest.mark.sphinx("html", testroot="html-context")
+@pytest.mark.sphinx("html", freshenv=True, testroot="html-context")
 def test(outdir: Path):
     """Test."""
     assert (outdir / "index.html").is_file()
