ddc0ff154a9b4acc5725ff3431b50c711e681bf5
Robpol86 <robpol86@gmail.com>
2022-04-28 14:07:35 -0500

    Disable directive when only one theme is used.
    Mitigates warnings created by the epub builder. Using this extension
    with one theme is also out of scope and should act as if the extension
    doesn't exist.

---
+++ tests/unit_tests/test_docs/test-toctree-directive/single/conf.py
@@ -0,0 +1,16 @@
+"""Sphinx test configuration."""
+import os
+
+from sphinx_multi_theme.theme import MultiTheme, Theme
+
+exclude_patterns = ["_build"]
+extensions = ["sphinx_multi_theme.multi_theme"]
+if os.environ.get("TEST_IN_SUBPROCESS") != "TRUE":
+    extensions.append("conftest_fork_exit_save_child_data")
+master_doc = "index"
+nitpicky = True
+html_theme = MultiTheme(
+    [
+        Theme("sphinx_rtd_theme", "Primary"),
+    ]
+)
